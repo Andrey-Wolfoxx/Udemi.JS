227cbfc9e711b81c4a593b4f2492768c2d08df58
Andrey Beloff <jandtai75@gmail.com>
2022-10-24 20:56:40 +0300

    10.24.22

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const numberOfFilms = +prompt('How many films have u seen?', '');
+
+let personalMovitDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+let a = prompt('One of the last film?', ''),
+    b = prompt('Rate it', ''),
+	c = prompt('One of the last film?', ''),
+	d = prompt('Rate it', '');
+
+personalMovitDB.movies[a] = b;
+personalMovitDB.movies[c] = d;
+
+console.log(personalMovitDB);
