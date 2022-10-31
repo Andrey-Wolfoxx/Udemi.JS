917ed801cf49f0e012783774dfeacabac1be6324
Andrey Beloff <jandtai75@gmail.com>
2022-10-31 19:19:15 +0300

    10.31.22 mon=vieApp

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
 'use strict';
 
-const usdCurr = 28;
-const eurCurr = 32;
+let numberOfFilms;
 
-function convert (amount, curr) {
-	console.log(curr * amount);
+
+function start() {
+	numberOfFilms = +prompt('How many films have u seen?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('How many films have u seen?', '');
+	} 
 }
 
-convert(500, usdCurr);
-convert(500, eurCurr);
+start();
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt('One of the last film?', ''),
+			b = prompt('Rate it', '');
+		
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		alert("You've seen a few films");
+	} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+		alert('You are common viewer');
+	} else if (personalMovieDB.count >= 30) {
+		alert('You are cinophile');
+	} else {
+		alert('Error!');
+	}
+}
+
+detectPersonalLevel();
+
+console.log(personalMovieDB);
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Your favorite genres numbered: ${i}`);
+	}
+}
+
+writeYourGenres();
