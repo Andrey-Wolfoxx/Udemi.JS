04e7a3e36b91c7f759487cdb65cd6959322e006f
Andrey Beloff <jandtai75@gmail.com>
2022-10-31 13:05:23 +0300

    10.31.22

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
 'use strict';
 
-let num = 20;
+const usdCurr = 28;
+const eurCurr = 32;
 
-function showFirstMessege(text) {
-	console.log(text);
-	let num = 10;
-	console.log(num);
+function convert (amount, curr) {
+	console.log(curr * amount);
 }
 
-showFirstMessege('Hello world');
-console.log(num);
-
-function culc(a, b) {
-	return (a + b);
-}
-
-//console.log(culc(4, 3));
-//console.log(culc(5, 6));
-
-function ret() {
-	let num = 50;
-	return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const loger = function () {
-	console.log('Hello');
-};
-
-loger();
-
-const calc = (a, b) => {
-	console.log('1');
-	return a + b;
-};
-
-console.log(calc(5, 6));
+convert(500, usdCurr);
+convert(500, eurCurr);
