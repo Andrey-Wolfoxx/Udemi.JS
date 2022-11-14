4d5eab9942ebc8dfb65f7d772b032fbd0222afec
Andrey Beloff <jandtai75@gmail.com>
2022-11-14 12:32:03 +0300

    11.14.22 arrays

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 'use strict';
 
-function first() {
-	setTimeout(function() {
-		console.log(1);
-	}, 500);
+const arr = [10, 2, 13, 26, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+	return a - b;
 }
 
-function second() {
-	console.log(2);
+arr[99] = 0;
+console.log(arr.length);
+
+arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
 }
 
-first();
-second();
-
-function learnJS(lang, callback) {
-	console.log(`I learn: ${lang}`);
-	callback();
+for (let value of arr) {
+	console.log(value);
 }
 
-function done() {
-	console.log('I learned this lesson');
-}
+arr.forEach(function(item, i, arr) {
+	console.log(`${i}: ${item} inside of array: ${arr}`);
+});
 
-learnJS('Javascript', done);
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; ')); 
+
