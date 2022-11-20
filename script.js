4ee5f81cc6b53fce1f915171d35288cbd5dee808
Andrey Beloff <jandtai75@gmail.com>
2022-11-20 20:13:08 +0300

    11.20.2022, поверхностные копии

--- script.js
+++ script.js
@@ -1,35 +1,95 @@
 'use strict';
 
-const arr = [10, 2, 13, 26, 8];
-arr.sort(compareNum);
-console.log(arr);
+//let a = 5,
+//	b = a;
 
-function compareNum(a, b) {
-	return a - b;
+//b = b + 5;
+
+//console.log(b, a);
+
+//const obj = {
+//	a: 5,
+//	b: 1
+//};
+
+//const copy = obj; //link
+
+//copy.a = 10;
+
+//console.log(copy, obj);
+
+function copy(mainObj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+
+	return objCopy;
 }
 
-arr[99] = 0;
-console.log(arr.length);
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+};
 
-arr.pop();
-arr.push(10);
+const newNumbers = copy(numbers);
 
-console.log(arr);
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
-for (let i = 0; i < arr.length; i++) {
-	console.log(arr[i]);
+//console.log(newNumbers);
+//console.log(numbers);
+
+const add = {
+	d: 17,
+	e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+//console.log(add);
+//console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[2] = 'isduofijsoi';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+	  blogs = ['wordpress', 'livejournal', 'blogger'],
+	  internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
 }
 
-for (let value of arr) {
-	console.log(value);
-}
+const num = [2, 5, 7];
 
-arr.forEach(function(item, i, arr) {
-	console.log(`${i}: ${item} inside of array: ${arr}`);
-});
+log(...num);
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join('; ')); 
+const array = ['a', 'b'];
 
+const newAarray = [...array];
+
+console.log(newAarray);
+
+const q = {
+	one: 1,
+	two: 2
+};
+
+const newObj = {...q};
