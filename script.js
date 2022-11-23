be1eba28b2db438b5ee5c2334a476e3a8e579f19
Andrey Beloff <jandtai75@gmail.com>
2022-11-23 23:36:49 +0300

    11.23.2022 App

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
 'use strict';
 
-let str = 'some';
-let strObj = new String(str);
+let personalMovieDB = {
+	count: 0,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+	start: function () {
+		this.count = +prompt('How many films have u seen?', '');
+	
+		while (this.count == '' || this.count == null || isNaN(this.count)) {
+			this.count= +prompt('How many films have u seen?', '');
+		} 
+	},
+	rememberMyFilms: function () {
+		for (let i = 0; i < 2; i++) {
+			let a = prompt('One of the last film?', '').trim(),
+				b = prompt('Rate it', '').trim();
+			
+			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+				this.movies[a] = b;
+				console.log('done');
+			} else {
+				console.log('error');
+				i--;
+			}
+		}
+	},
+	detectPersonalLevel: function () {
+		if (this.count < 10) {
+			alert("You've seen a few films");
+		} else if (this.count >=10 && this.count < 30) {
+			alert('You are common viewer');
+		} else if (this.count >= 30) {
+			alert('You are cinophile');
+		} else {
+			alert('Error!');
+		}
+	},
+	showMyDB: function (hidden) {
+		if (!hidden) {
+			console.log(personalMovieDB);
+		}
+	},
+	toggleVisibleMyDB: function () {
+		this.privat ? this.privat = false : this.privat = true; 
+	},
+	writeYourGenres: function () {
+		for (let i = 0; i < 1; i++) {
+			//let a = prompt(`Your favorite genres numbered: ${i + 1}`).trim();
+			//if (a != null || a != '') {
+			//	this.genres[i] = a;
+			//} else {
+			//	console.log('Incorrect data');
+			//	i--;
+			//}
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+			let genres = prompt(`Введите ваши любимвые жанры через запятую`).trim().toLowerCase();
+			if (genres != null || genres != '') {
+				this.genres = genres.split(', ');
+				this.genres.sort();
+			} else {
+				console.log('Incorrect data');
+				i--;
+			}
+		}
 
-console.dir([1, 2, 3]);
-
-const soldier = {
-	health: 400,
-	armor: 100,
-	sayHello: function () {  
-		console.log('Hello!');
+		this.genres.forEach((item, i) => {
+			console.log(`Любимый жанр ${i + 1} - это ${item}`);
+		});
 	}
 };
 
-const john = Object.create(soldier);
-
-//const john = {
-//	health: 100
-//};
-
-//john._proto_ = soldier;
-
-//Object.setPrototypeOf(john, soldier);
-
-//console.log(john.armor);
-john.sayHello();
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
