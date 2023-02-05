66c9c8b28d7ddd6827532ef3d8abf41c9e5f687e
Andrey Beloff <jandtai75@gmail.com>
2023-02-05 23:20:44 +0300

    Recurtion

--- js/script.js
+++ js/script.js
@@ -1,21 +1,111 @@
 'use strict';
 
-//console.log(document.body);
-//console.log(document.documentElement);
-//console.log(document.body.childNodes);
-//console.log(document.body.firstChild);
-//console.log(document.body.firstElementChild);
-//console.log(document.body.lastChild);
+//function pow(x, n) {
+//	let result = 1;
 
-//console.log(document.querySelector('#current').parentNode.parentNode);
-//console.log(document.querySelector('#current').parentNode.parentElement);
+//	for (let i = 0; i < n; i++) {
+//		result *= x;
+//	}
+//	return result;
+//}
 
-//console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+//function pow(x, n) {
+//	if (n === 0) {
+//		return 1;
+//	} else {
+//		x *= pow(x, n - 1);
+//	}
+//	return x;
+//}
 
-for (let node of document.body.childNodes) {
-	if (node.nodeName == '#text') {
-		continue;
+//console.log(pow(2, 1)); // 2
+//console.log(pow(2, 2)); // 4
+//console.log(pow(2, 3)); // 8
+//console.log(pow(2, 4)); // 16
+
+let students = {
+	js: [{
+		name: 'John',
+		progress: 100
+	},
+	{
+		name: 'Ivan',
+		progress: 60
+	}],
+
+	html: {
+		basic: [{
+			name: 'Peter',
+			progress: 20
+		},
+		{
+			name: 'Ann',
+			progress: 18
+		}],
+
+		pro: [{
+			name: 'Sam',
+			progress: 10
+		}],
+
+		semi: {
+			students: [{
+				name: 'Test',
+				progress: 100
+			}]
+		}
+	}
+};
+
+function getTotalProgressByIteretin(data) {
+	let total = 0;
+	let students = 0;
+
+	for (let course of Object.values(data)) {
+		if (Array.isArray(course)) {
+			students += course.length;
+
+			for (let i = 0; i < course.length; i++) {
+				total += course[i].progress;
+			}
+		} else {
+			for (let subCourse of Object.values(course)) {
+				students += subCourse.length;
+				
+				for (let i = 0; i < subCourse.length; i++) {
+					total += subCourse[i].progress;
+				}
+			}
+		}
 	}
 
-	console.log(node);
+	return total / students;
 }
+
+//console.log(getTotalProgressByIteretin(students));
+
+
+function getTotalProgressByRecurtion(data) {
+	if (Array.isArray(data)) {
+		let total = 0;
+
+		for (let i = 0; i < data.length; i++) {
+			total += data[i].progress;
+		}
+
+		return [total, data.length];
+	} else {
+		let total = [0, 0];
+		
+		for (let subData of Object.values(data)) {
+			const subDataArr = getTotalProgressByRecurtion(subData);
+			total[0] += subDataArr[0];
+			total[1] += subDataArr[1];
+		}
+
+		return total;
+	}
+}
+
+const result = getTotalProgressByRecurtion(students);
+console.log(result[0] / result[1]);
